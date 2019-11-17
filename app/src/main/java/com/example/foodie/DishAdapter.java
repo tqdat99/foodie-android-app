846f1fe16946e78ad4cbd280e0fee39f1b847ed9
tqdat99 <56955642+tqdat99@users.noreply.github.com>
2019-11-17 15:33:47 +0700

    Yêu thích

--- app/src/main/java/com/example/foodie/DishAdapter.java
+++ app/src/main/java/com/example/foodie/DishAdapter.java
@@ -1,69 +1,70 @@
-//package com.example.myapplication;
-//
-//import android.content.Context;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.BaseAdapter;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import java.util.List;
-//
-//public class DishAdapter extends BaseAdapter {
-//
-//    private Context context;
-//    private int layout;
-//    private List<Dish> dishList;
-//
-//    public DishAdapter(Context context, int layout, List<Dish> dishList) {
-//        this.context = context;
-//        this.layout = layout;
-//        this.dishList = dishList;
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return dishList.size();
-//    }
-//
-//    @Override
-//    public Object getItem(int i) {
-//        return dishList.get(i);
-//    }
-//
-//    @Override
-//    public long getItemId(int i) {
-//        return i;
-//    }
-//
-//    private class ViewHolder {
-//        ImageView holder_thumbnail;
-//        TextView holder_name, holder_type, holder_ingredient;
-//    }
-//
-//    @Override
-//    public View getView(int i, View view, ViewGroup viewGroup) {
-//        ViewHolder holder;
-//        if (view == null) {
-//            holder = new ViewHolder();
-//            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//            view = inflater.inflate(layout, null);
-//            holder.holder_thumbnail = (ImageView) view.findViewById(R.id.row_iv_thumbnail);
-//            holder.holder_name = (TextView) view.findViewById(R.id.row_tv_name);
-//            holder.holder_type = (TextView) view.findViewById(R.id.row_tv_type);
-//            holder.holder_ingredient = (TextView) view.findViewById(R.id.row_tv_ingredient);
-//            view.setTag(holder);
-//        } else {
-//            holder = (ViewHolder) view.getTag();
-//        }
-//        Dish dish = dishList.get(i);
-//
-//        holder.holder_thumbnail.setImageResource(dish.getThumbnail());
-//        holder.holder_name.setText(dish.getName());
-//        holder.holder_type.setText(dish.getType());
-//        holder.holder_ingredient.setText(dish.getIngredient());
-//
-//        return view;
-//    }
-//}
+package com.example.foodie;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.foodie.Dish;
+import com.example.foodie.R;
+
+import java.util.List;
+
+public class DishAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private List<Dish> dishList;
+
+    public DishAdapter(Context context, int layout, List<Dish> dishList) {
+        this.context = context;
+        this.layout = layout;
+        this.dishList = dishList;
+    }
+
+    @Override
+    public int getCount() {
+        return dishList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return dishList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    private class ViewHolder {
+        ImageView holder_thumbnail;
+        TextView holder_name, holder_type, holder_ingredient;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if (view == null) {
+            holder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(layout, null);
+            holder.holder_thumbnail = (ImageView) view.findViewById(R.id.row_iv_thumbnail);
+            holder.holder_name = (TextView) view.findViewById(R.id.row_tv_name);
+            holder.holder_type = (TextView) view.findViewById(R.id.row_tv_type);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        Dish dish = dishList.get(i);
+
+        holder.holder_thumbnail.setImageResource(dish.getThumbnail());
+        holder.holder_name.setText(dish.getName());
+        holder.holder_type.setText(dish.getType());
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/foodie/ui/store/StoreFragment.java
+++ app/src/main/java/com/example/foodie/ui/store/StoreFragment.java
@@ -1,26 +1,91 @@
 package com.example.foodie.ui.store;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.foodie.DatabaseHelper;
+import com.example.foodie.Dish;
+import com.example.foodie.DishAdapter;
 import com.example.foodie.R;
 
-public class StoreFragment extends Fragment {
-//test
-    private StoreViewModel storeViewModel;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class StoreFragment extends ListFragment {
+
+    View root;
+    ArrayList<Dish> dishArrayList;
+    DishAdapter adapter;
+
+    //Biến đọc db
+    private static final String DATABASE_NAME = "DBMonAn.db";
+    private static final int DATABASE_VERSION = 1;
+    private DatabaseHelper databaseHelper;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        storeViewModel =
-                ViewModelProviders.of(this).get(StoreViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_store, container, false);
+        root = inflater.inflate(R.layout.fragment_store, container, false);
+
+        dishArrayList = new ArrayList<>();
+
+        //Chỗ này đọc file db trong asset r copy vô database (nghĩa là copy cái file db trong asset rồi tạo 1 cái y chang vô folder của ứng dụng)
+        databaseHelper = new DatabaseHelper(getActivity());
+        File database = getActivity().getApplication().getDatabasePath(DatabaseHelper.DBNAME);
+        if (database.exists() == false){
+            databaseHelper.getReadableDatabase();
+            if (copyDatabase(getActivity())){
+                Toast.makeText(getActivity(), "Copy success", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        //Lấy ra list món ăn từ db
+        dishArrayList = new ArrayList<>();
+        dishArrayList = databaseHelper.getDishList();
+
+        //Toast ra số món lấy đc (để test thôi)
+        Toast.makeText(getActivity(), "" + dishArrayList.size(), Toast.LENGTH_SHORT).show();
+
+        adapter = new DishAdapter(getActivity(), R.layout.row_dish, dishArrayList);
+        setListAdapter(adapter);
+
 
         return root;
     }
+
+    //Copy db từ trong asset vô ứng dụng
+    private boolean copyDatabase(Context context){
+        try{
+            InputStream inputStream = context.getAssets().open(DatabaseHelper.DBNAME);
+            String outFileName = DatabaseHelper.DBLOCATION + DatabaseHelper.DBNAME;
+            OutputStream outputStream = new FileOutputStream(outFileName);
+            byte[]buff = new byte[1024];
+            int length = 0;
+            while ((length = inputStream.read(buff)) > 0){
+                outputStream.write(buff, 0, length);
+            }
+            outputStream.flush();
+            outputStream.close();
+            Log.w("MainActivity", "DB Copied");
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
